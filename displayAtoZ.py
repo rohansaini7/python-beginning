2703afd44c1702d54f881097df715b47e010e400
rohansaini7 <sainirohan23@gmail.com>
2020-01-14 23:41:33 +0530

    adding displayAtoz and greatest of 3

--- displayAtoZ.py
+++ displayAtoZ.py
@@ -1,5 +1,6 @@
 #print a 2 z
 for i in range(ord('A'),ord('Z')+1):
 
-    print(chr(i))
+    print(chr(i)+"\t"+chr(i).lower(),end=" ")
+
 
---
+++ greatestof3.py
@@ -0,0 +1,18 @@
+#to find greatest number out of threee
+a=int(input("enetr first"))
+b=int(input("enetr second"))
+c=int(input("enetr third"))
+if a > b :
+
+    if a > c:
+         print(a,"is greatest")
+
+    else:
+        print(c, "is greatest")
+
+else:
+    if b > c:
+        print(b , 'is greatest')
+    else:
+        print(c , " is greatest")
+
