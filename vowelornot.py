710c1325c16a89d41c394d65abd93703a658b11d
rohansaini7 <sainirohan23@gmail.com>
2020-01-15 00:03:34 +0530

    adding vowelsornot

---
+++ vowelornot.py
@@ -0,0 +1,6 @@
+#to find char is vowel or not
+x=input("enetr alphabet to check it is vowel or not")
+if x == 'a'or x == 'e'or x == 'i'or x == 'u'or x == 'A'or x == 'E'or x == 'I'or x == 'O'or x == 'U':
+    print("vowel")
+else:
+    print("not vowel")
